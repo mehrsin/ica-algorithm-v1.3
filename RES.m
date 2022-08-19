1fb11fad227080a68d768b4bc21a7b6f37920f15
mehrsin <96544087+mehrsin@users.noreply.github.com>
2022-08-19 22:54:41 +0430

    Update RES.m

--- RES.m
+++ RES.m
@@ -10,11 +10,3 @@ xlswrite('out.xlsx',sol.info.O,1,'d5');
 
 
 end
-
-
-% in mahsol motalegh be matlabhome.ir ast foroshe an dar saiere sitha gheire ghanoni ast
-% va peigard ghanoni darad dar sorate moshahede lotfan be ma etela dahid
-% https://matlabhome.ir/
-% @matlabhome_admin
-% https://t.me/matlabhome
-% 09190090258
